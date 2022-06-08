2eecb42346e78474cd2e6659b35a71040e4db266
manish <manishgiri562>
2022-06-08 21:50:49 +0530

    added buy options for mutual fund

--- backend/controller/investmentController.js
+++ backend/controller/investmentController.js
@@ -1,68 +1,76 @@
 const Order = require("../models/investmentModel");
 
 exports.getAllOrders = async (req, res, next) => {
-  try {
-     const orders = req.params.type==='all'? 
-                        await  Order.find({user:req.params.userId}).sort({date:-1}):
-                        await  Order.find({user:req.params.userId,type:req.params.type}).sort({date:-1});
-    //console.log(req.params.type);   
-    //const orders= await Order.find({user:req.params.userId}).sort({date:-1});                  
-    return res.status(200).json({
-      msg: "Success",
-      orders
-    });
-  } catch (err) {
-    return res.status(404).json({
-      message: err.message,
-      err,
-    });
-  }
+    try {
+        const orders =
+            req.params.type === "all"
+                ? await Order.find({ user: req.params.userId }).sort({
+                      date: -1,
+                  })
+                : await Order.find({
+                      user: req.params.userId,
+                      type: req.params.type,
+                  }).sort({ date: -1 });
+        //console.log(req.params.type);
+        //const orders= await Order.find({user:req.params.userId}).sort({date:-1});
+        return res.status(200).json({
+            msg: "Success",
+            orders,
+        });
+    } catch (err) {
+        return res.status(404).json({
+            message: err.message,
+            err,
+        });
+    }
 };
 
 exports.getIdOrders = async (req, res, next) => {
-  try {
-     const orders = await  Order.find({user:req.params.userId,stock:req.params.productId}).sort({date:-1});
-    //console.log(req.params.productId);   
-    //const orders= await Order.find({user:req.params.userId}).sort({date:-1});                  
-    return res.status(200).json({
-      msg: "Success",
-      orders
-    });
-  } catch (err) {
-    return res.status(404).json({
-      message: err.message,
-      err,
-    });
-  }
+    try {
+        const orders = await Order.find({
+            user: req.params.userId,
+            stock: req.params.productId,
+        }).sort({ date: -1 });
+        //console.log(req.params.productId);
+        //const orders= await Order.find({user:req.params.userId}).sort({date:-1});
+        return res.status(200).json({
+            msg: "Success",
+            orders,
+        });
+    } catch (err) {
+        return res.status(404).json({
+            message: err.message,
+            err,
+        });
+    }
 };
-
 
 exports.createOrder = async (req, res, next) => {
-  try {
-    const order = await Order.create(req.body);
-    return res.status(200).json({
-      msg: "Success",
-      order
-    });
-  } catch (err) {
-    return res.status(404).json({
-      message: "Error while creating order",
-      err,
-    });
-  }
+    try {
+        const order = await Order.create(req.body);
+        return res.status(200).json({
+            msg: "Success",
+            order,
+        });
+    } catch (err) {
+        return res.status(404).json({
+            message: "Error while creating order",
+            err,
+        });
+    }
 };
 exports.getOrderById = async (req, res, next) => {
-  try {
-    const order = await Order.findById(req.params.orderId);
-    return res.status(200).json({
-      msg: "Success",
-      order,
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(404).json({
-      message: "Error while geeting resposne",
-      err,
-    });
-  }
+    try {
+        const order = await Order.findById(req.params.orderId);
+        return res.status(200).json({
+            msg: "Success",
+            order,
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(404).json({
+            message: "Error while geeting resposne",
+            err,
+        });
+    }
 };
--- frontend/src/components/mutualFunds/mutualFundDetail.js
+++ frontend/src/components/mutualFunds/mutualFundDetail.js
@@ -2,22 +2,29 @@ import React, { useState, useEffect } from "react";
 import { Button, IconButton } from "@mui/material";
 import ShareIcon from "@mui/icons-material/Share";
 import BookmarkAddOutlinedIcon from "@mui/icons-material/BookmarkAddOutlined";
-
+import { getUser } from "../../api/users.api";
+import AuthStore from "./../../middleware/authstore";
+import Snackbars from "../Snackbars";
+import { makeInvestment } from "../../api/investment.api";
 import "./mutualFundDetail.css";
 import { useParams } from "react-router-dom";
 
 const MutualFundDetail = () => {
-    localStorage.setItem("route","mutual-funds-user-invest");
     const [mf, setMf] = useState({});
+    const [sip, setSip] = useState(0);
+    const [snackopen, setsnack] = useState(false);
+    const [snackmsg, setsnackmsg] = useState("");
+    const [snacktype, setsnacktype] = useState("success");
+    localStorage.setItem("route", "mutual-funds-user-invest");
     const { id } = useParams();
-    localStorage.setItem("productID",id);
-    localStorage.setItem("productType","mutual-funds");
+    const userId = AuthStore.getUserDetail()._id;
+    const token = localStorage.jwToken;
+    localStorage.setItem("productID", id);
+    localStorage.setItem("productType", "mutual-funds");
     useEffect(() => {
         const apiCall = async () => {
             try {
-                const data = await fetch(
-                    `/api/v1/mutual-funds/${id}`
-                );
+                const data = await fetch(`/api/v1/mutual-funds/${id}`);
                 const mfData = await data.json();
                 setMf((mf) => mfData);
             } catch (err) {
@@ -27,61 +34,153 @@ const MutualFundDetail = () => {
         apiCall();
     }, [id]);
 
+    const checkUser = () => {
+        if (sip >= mf.minSipAmount) {
+            getUser(token, userId).then((data) => {
+                if (data.hasOwnProperty("msg")) {
+                    AuthStore.clearJWT();
+                    window.location.href = "/";
+                } else {
+                    if (!data.kyc) {
+                        setsnacktype("error");
+                        setsnackmsg(
+                            "Sorry, you cannot invest in the stock now, as your KYC is incomplete."
+                        );
+                        setsnack(true);
+                    } else {
+                        buyMutualFund();
+                    }
+                }
+            });
+        } else {
+            console.log("error");
+            setsnacktype("error");
+            setsnackmsg(
+                `Sorry, SIP amount must be grater than ${mf.minSipAmount}!!`
+            );
+            setsnack(true);
+        }
+    };
+
+    const buyMutualFund = () => {
+        //here the type must be equal to the type description in investment model Stock/Mutual-Fund/Fixed-Deposit
+        makeInvestment(
+            token,
+            userId,
+            id,
+            mf.title,
+            "Mutual Fund",
+            `${sip} Per month`,
+            sip
+        ).then((data) => {
+            if (data.hasOwnProperty("msg") && data["msg"] === "Success") {
+                setSip(1);
+                setsnacktype("success");
+                setsnackmsg("Your investment is successful.");
+                setsnack(true);
+            } else {
+                setsnacktype("error");
+                setsnackmsg("Investment failed.");
+                setsnack(true);
+            }
+            //console.log(data);
+        });
+    };
+
     if (!mf) {
         return <p>404 Not Found</p>;
     }
 
     return (
-        <div className="mutual_fund">
-            <div className="product__header">
-                <img
-                    className="product__logo"
-                    alt="product-image"
-                    src={
-                        mf.image ||
-                        "https://groww.in/images/partners/icici_groww.svg"
-                    }
-                />
-                <div>
-                    <IconButton className="add__sicon">
-                        <BookmarkAddOutlinedIcon
-                            style={{ margin: "0px 10px" }}
-                            fontSize="large"
+        <div className="wrapper" style={{ width: "100%", minHeight: "100vh" }}>
+            <div className="mutual_fund">
+                <div className="product__header">
+                    <img
+                        className="product__logo"
+                        alt="product-image"
+                        src={
+                            mf.image ||
+                            "https://groww.in/images/partners/icici_groww.svg"
+                        }
+                    />
+                    <div>
+                        <IconButton className="add__sicon">
+                            <BookmarkAddOutlinedIcon
+                                style={{ margin: "0px 10px" }}
+                                fontSize="large"
+                            />
+                        </IconButton>
+                        <IconButton className="add__iscon">
+                            <ShareIcon fontSize="large" />
+                        </IconButton>
+                    </div>
+                </div>
+                <div className="mf__title">
+                    <h2>{mf.title}</h2>
+                </div>
+                <div className="mf__tags">
+                    {/* {mf && mf.tags.map((tag) => <button>{tag}</button>)} */}
+                </div>
+                <div className="fundamental">
+                    <div className="fundamental__box">
+                        <div className="fundamental_item">
+                            <p className="field">Nav: 20 May 2022</p>
+                            <p className="value">₹{mf.nav}</p>
+                        </div>
+                        <div className="fundamental_item">
+                            <p className="field">Min SIP amount</p>
+                            <p className="value">₹{mf.minSipAmount}</p>
+                        </div>
+                    </div>
+                    <div className="fundamental__box">
+                        <div className="fundamental_item">
+                            <p className="field">Ratings</p>
+                            <p className="value">{mf.rating}</p>
+                        </div>
+                        <div className="fundamental_item">
+                            <p className="field">Fund Size</p>
+                            <p className="value">₹{mf.fundSize}Cr</p>
+                        </div>
+                    </div>
+                </div>
+                <form>
+                    <div className="form__controls">
+                        <label>SIP Amount</label>
+                        <input
+                            type="number"
+                            min="100"
+                            step="100"
+                            val={sip}
+                            onChange={(e) => {
+                                setSip(e.target.value);
+                            }}
+                            className="form__input"
+                            required
                         />
-                    </IconButton>
-                    <IconButton className="add__iscon">
-                        <ShareIcon fontSize="large" />
-                    </IconButton>
-                </div>
-            </div>
-            <div className="mf__title">
-                <h2>{mf.title}</h2>
-            </div>
-            <div className="mf__tags">
-                {/* {mf && mf.tags.map((tag) => <button>{tag}</button>)} */}
-            </div>
-            <div className="fundamental">
-                <div className="fundamental__box">
-                    <div className="fundamental_item">
-                        <p className="field">Nav: 20 May 2022</p>
-                        <p className="value">₹{mf.nav}</p>
                     </div>
-                    <div className="fundamental_item">
-                        <p className="field">Min SIP amount</p>
-                        <p className="value">₹{mf.minSipAmount}</p>
+                    <div className="form__controls">
+                        <Button
+                            variant="contained"
+                            style={{ backgroundColor: "rgb(46, 221, 136)" }}
+                            fullWidth={true}
+                            size="large"
+                            onClick={checkUser}
+                        >
+                            BUY
+                        </Button>
                     </div>
-                </div>
-                <div className="fundamental__box">
-                    <div className="fundamental_item">
-                        <p className="field">Ratings</p>
-                        <p className="value">{mf.rating}</p>
-                    </div>
-                    <div className="fundamental_item">
-                        <p className="field">Fund Size</p>
-                        <p className="value">₹{mf.fundSize}Cr</p>
-                    </div>
-                </div>
+                    <p className="foot">100% SAFE AND SECURE</p>
+                </form>
             </div>
+            {snackopen && (
+                <Snackbars
+                    msg={snackmsg}
+                    type={snacktype}
+                    close={() => {
+                        setsnack(false);
+                    }}
+                />
+            )}
         </div>
     );
 };
